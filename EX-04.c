9e70e7c2773dd1928c3c2548ea61d3357b4d05ff
gem install selenium-webdriver <brendojnio@gmail.com>
2017-10-24 14:59:50 -0200

    Exercicio 4

--- EX-04.c
+++ EX-04.c
@@ -3,23 +3,34 @@
 #include <time.h>
 
 int main(){
-int vetor[10], i, maior, menor;
+int vetor[10], i, maior = 0, menor = 100000000000, posicao_maior = 0, posicao_menor = 0;
 
 srand(time(0));
 
 for(i = 0; i < 10; i++){
 vetor[i] = rand() % 100;
-    maior = vetor[0];
-    menor = vetor[0];
+}
+
+for(i = 0; i < 10; i++){
+printf("%d ", vetor[i]);
+}
+
+printf("\n\n");
+
+for(i = 0; i < 10; i++){
+
 if(vetor[i] > maior){
     maior = vetor[i];
+    posicao_maior = i + 1;
 }
 if(vetor[i] < menor){
     menor = vetor[i];
+    posicao_menor = i + 1;
 }
-}
-printf("O menor valor e: %d\n", menor);
-printf("O maior valor e: %d", maior);
 }
 
+printf("O menor valor e: %d e sua posicao e: %d\n", menor, posicao_menor);
+printf("O maior valor e: %d e sua posicao e: %d", maior, posicao_maior);
+
+}
 
